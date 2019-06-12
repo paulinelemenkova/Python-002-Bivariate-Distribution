95562e9cd44415e915e08fec6ff02a844e3ee2b6
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-12 17:56:35 +0800

    Add files via upload

--- Script-002-BivarDistr.py
+++ Script-002-BivarDistr.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import seaborn as sb
 from matplotlib import pyplot as plt
 import pandas as pd
@@ -13,11 +10,3 @@ dfM = pd.read_csv("Tab-Morph.csv")
 dfM.head(5)
 sb.jointplot(x='igneous_volc',y='sedim_thick',data=dfM)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-import seaborn as sb
